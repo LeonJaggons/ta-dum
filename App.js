10212992d370e87f47ddbfe582ec5c3c593f32e2
Leon Jaggon <leonojaggon@gmail.com>
2022-02-13 17:11:04 -0500

    Add home skeleton.

--- App.js
+++ App.js
@@ -3,33 +3,18 @@ import "./firebase-api/firebase-firestore.js";
 import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import {
-    Box,
-    Center,
-    Flex,
-    HStack,
-    NativeBaseProvider,
-    Text,
-    VStack,
-} from "native-base";
-import { Ionicons } from "@expo/vector-icons";
-import {
-    auth,
-    createTaDumUserFromGoogle,
-    createTDID,
-    isTaDumUser,
-    signInTaDumUser,
-    signInWithGoogle,
-} from "./firebase-api/firebase-auth.js";
-
+import { Center, Flex, NativeBaseProvider } from "native-base";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Home from "./components/sign-in/home/Home.jsx";
 export default function App() {
     return (
         <Provider store={store}>
             <NativeBaseProvider>
                 <Flex h={"100%"} safeArea>
-                    <Center flexGrow={1}>APP</Center>
+                    <Center flexGrow={1}>
+                        <Home />
+                    </Center>
                 </Flex>
                 <StatusBar style="auto" />
             </NativeBaseProvider>
---
+++ components/sign-in/home/Home.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from 'native-base'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <Box>
+            <Heading>HOME</Heading>
+        </Box>
+    )
+}
+
+export default Home
